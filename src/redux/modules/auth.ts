a7cd9c273cd1b0b8a7a516bc94f0f532764422cb
조명환 <idmash88@gmail.com>
2023-04-02 23:47:38 +0900

    로그인 로그아웃 기능 추가

---
+++ src/redux/modules/auth.ts
@@ -0,0 +1,78 @@
+import { push } from "connected-react-router";
+import { Action, createActions, handleActions } from "redux-actions";
+import { call, put, select, takeEvery } from "redux-saga/effects";
+import TokenService from "../../services/TokenServices";
+import UserService from "../../services/UserServices";
+import { AuthState, LoginReqtype } from "../../types";
+
+const initialstate: AuthState = {
+  token: null,
+  loading: false,
+  error: null,
+};
+
+const prefix = "my-books/auth";
+
+export const { pending, success, fail } = createActions(
+  "PENDING",
+  "SUCCESS",
+  "FAIL",
+  { prefix }
+);
+
+const reducer = handleActions<AuthState, string>(
+  {
+    PENDING: (state) => ({
+      ...state,
+      loading: true,
+      error: null,
+    }),
+    SUCCESS: (state, action) => ({
+      token: action.payload,
+      loading: false,
+      error: null,
+    }),
+    FAIL: (state, action: any) => ({
+      ...state,
+      loading: false,
+      error: action.payload,
+    }),
+  },
+  initialstate,
+  { prefix }
+);
+
+export default reducer;
+
+// saga
+export const { login, logout } = createActions("LOGIN", "LOGOUT", { prefix });
+function* loginSaga(action: Action<LoginReqtype>) {
+  try {
+    yield put(pending());
+    const token: string = yield call(UserService.login, action.payload);
+    TokenService.set(token);
+    yield put(success(token));
+    yield put(push("/"));
+  } catch (error: any) {
+    yield put(fail(new Error(error?.response?.data?.error || "UNKNOWN_ERROR")));
+  }
+}
+
+function* logoutSaga() {
+  try {
+    yield put(pending());
+    const token: string = yield select((state) => state.auth.token);
+    yield call(UserService.logout, token);
+    TokenService.set(token);
+    yield put(success(token));
+  } catch (error: any) {
+  } finally {
+    TokenService.remove();
+    yield put(success(null));
+  }
+}
+
+export function* authSaga() {
+  yield takeEvery(`${prefix}/LOGIN`, loginSaga);
+  yield takeEvery(`${prefix}/LOGOUT`, logoutSaga);
+}
